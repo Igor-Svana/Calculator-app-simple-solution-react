53895e7a917b5c5b62932d8651c218c807e370f2
Igor-Svana <igor.svana@gmail.com>
2023-03-27 17:27:35 +0200

    fix

--- src/components/toggle/toggle.jsx
+++ src/components/toggle/toggle.jsx
@@ -6,20 +6,20 @@ const Toggle = ({setTheme, theme}) => {
         <div className='toggle-container'>
             <p>Theme</p>
             <div className='toggle' style={{background: theme.toggle_bg}}>
-              <div className='dot-container' name="dot-container" onClick={() => setTheme(Theme.dark_blue)}>
+              <div className='dot-container' id="dot-container" onClick={() => setTheme(Theme.dark_blue)}>
                 <label className="dot-label" htmlFor="dot-container">1</label>
               <span className='dot'style={{background: theme.toogle_color, visibility: theme !==Theme.dark_blue &&  'hidden'}} ></span>
               </div>
-              <div className='dot-container' onClick={() =>setTheme(Theme.light_gray)}>
+              <div className='dot-container' id="dot-container" onClick={() =>setTheme(Theme.light_gray)}>
               <label className="dot-label" htmlFor="dot-container">2</label>
               <span className='dot'style={{background: theme.toogle_color, visibility: theme !==Theme.light_gray &&  'hidden'}}></span>
-              </div>
-              <div className='dot-container' onClick={() => setTheme(Theme.dark_violet)}>
+              </div> 
+              <div className='dot-container' id="dot-container" onClick={() => setTheme(Theme.dark_violet)}>
               <label className="dot-label" htmlFor="dot-container">3</label>
               <span className='dot'style={{background: theme.toogle_color, visibility: theme !==Theme.dark_violet &&  'hidden'}} ></span>
               </div>             
             </div>
-        </div>
+        </div> 
     )
 }
 
